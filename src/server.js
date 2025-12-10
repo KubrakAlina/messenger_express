cc85c9a45f30fbdc2f37a002f0950f8d060008dd
Alina <alinakubikalex@gmail.com>
2025-12-10 19:15:20 +0100

    add swagger

--- src/server.js
+++ src/server.js
@@ -1,4 +1,42 @@
 const app = require('./app');
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
 
 const PORT = process.env.PORT || 3000;
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+      description: 'API documentation using Swagger',
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  apis: ['./src/routes/*.js'], // Path to your API docs
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
+
+
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
