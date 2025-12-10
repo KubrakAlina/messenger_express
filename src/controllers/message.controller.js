fee366df69e9cde6f69c43d770e54d9d4a2ae370
Alina <alinakubikalex@gmail.com>
2025-12-10 16:52:58 +0100

    fex message.controller.js

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,14 +1,14 @@
 const messageService = require("../services/message.service");
 
 async function getAllMessages(req, res) {
-  const users = await messageService.getAllMessages();
-  res.json(users);
+  const messages = await messageService.getAllMessages();
+  res.json(messages);
 }
 
 async function getMessageForChat(req, res) {
-  const user = await messageService.findMessagesForChat(req.params.chat_id);
-  if (!user) return res.status(404).json({ message: "Messages not found" });
-  res.json(user);
+  const message = await messageService.findMessagesForChat(req.params.chat_id);
+  if (!message) return res.status(404).json({ message: "Messages not found" });
+  res.json(message);
 }
 
 module.exports = {
