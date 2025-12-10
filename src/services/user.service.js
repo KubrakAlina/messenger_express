30092896188b5e5a5c47d73252e78104d94405f6
Alina <alinakubikalex@gmail.com>
2025-12-10 14:22:11 +0100

    fix formating

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,16 +1,16 @@
 const { readDB, writeDB } = require("../models/db.js");
 
 async function getAllUsers() {
-    const db = await readDB();
-    return db.users;
+  const db = await readDB();
+  return db.users;
 }
 
 async function findUserByName(name) {
-    const db = await readDB();
-    return db.users.find(u => u.username === name);
+  const db = await readDB();
+  return db.users.find(u => u.username === name);
 }
 
 module.exports = {
-    getAllUsers,
-    findUserByName
+  getAllUsers,
+  findUserByName
 };
